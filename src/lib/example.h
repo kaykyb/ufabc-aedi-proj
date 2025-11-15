cf519634330641de604c55732ccf844aec4f63a6
Kayky de Brito <oss@kaykyb.com>
2025-11-15 16:15:47 -0300

    Add tests

---
+++ src/lib/example.h
@@ -0,0 +1,10 @@
+//
+// Created by Kayky de Brito on 15/11/25.
+//
+
+#ifndef UFABC_AEDI_PROJ_EXAMPLE_H
+#define UFABC_AEDI_PROJ_EXAMPLE_H
+
+int add_one (int val);
+
+#endif //UFABC_AEDI_PROJ_EXAMPLE_H
