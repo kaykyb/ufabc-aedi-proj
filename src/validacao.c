d504bbdf8aa829588335e93ca99e7a35075681ff
Kayky de Brito <oss@kaykyb.com>
2025-11-29 23:22:04 -0300

    Corrige indentação no validacao.c

--- src/validacao.c
+++ src/validacao.c
@@ -2,39 +2,39 @@
 
 bool valida_cpf(char *cpf)
 {
-  for (int i = 0; i < 11; i++)
-    if (cpf[i] < '0' || cpf[i] > '9')
-      return false;
+    for (int i = 0; i < 11; i++)
+        if (cpf[i] < '0' || cpf[i] > '9')
+            return false;
 
-  return true;
+    return true;
 }
 
 bool valida_idade(int idade)
 {
-  return idade >= 18 && idade <= 120;
+    return idade >= 18 && idade <= 120;
 }
 
 bool valida_anos_cnh(int anos_cnh, int idade)
 {
-  return anos_cnh >= 0 && anos_cnh <= (idade - 18);
+    return anos_cnh >= 0 && anos_cnh <= (idade - 18);
 }
 
 bool valida_inadimplente(int inadimplente)
 {
-  return inadimplente == 0 || inadimplente == 1;
+    return inadimplente == 0 || inadimplente == 1;
 }
 
 bool valida_acidentes(int acidentes)
 {
-  return acidentes >= 0;
+    return acidentes >= 0;
 }
 
 bool valida_gastos(float gastos)
 {
-  return gastos >= 0;
+    return gastos >= 0;
 }
 
 bool valida_dias_alugados(int dias)
 {
-  return dias >= 0 && dias <= 366;
+    return dias >= 0 && dias <= 366;
 }
